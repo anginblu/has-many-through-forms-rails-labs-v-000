a90ecdc18304c510d155866f230dd8aac934780f
Jacqueline Lee <anginblu@gmail.com>
2018-01-31 02:18:53 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,7 @@ class Comment < ActiveRecord::Base
   accepts_nested_attributes_for :user
 
   def user_attributes=(user_attributes)
-    user_attributes.each do |i, user_attribute|
+    user_attributes.each do |name, user_attribute|
       unless user_attribute == ""
         user = User.find_or_create_by(i: user_attribute)
         self.user = user
