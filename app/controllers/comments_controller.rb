2712706ff289dc5b5e7a787d00297fbbfe0a009d
Jacqueline Lee <anginblu@gmail.com>
2018-01-31 01:35:20 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.create(comment_params)
-    if @comment.update()
+    if params[]@comment.update()
     redirect_to comment_path(@comment)
   end
 
